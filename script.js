9f008086674db465a159fe60faed838195a782db
Kim Pykhov <pykhovk@gmail.com>
2025-05-21 12:24:59 +0300

    function for a list of tasks forming

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-console.log("duck");
+function addTask(event) {
+    // prevent page reboot
+    event.preventDefault();
+
+    // storing value from input
+    let inputValue = document.querySelector("#task-input").value;
+    
+    // storing value from list
+    let taskList = document.querySelector("#task-list");
+
+    // adding the new value to the list
+    let newEl = document.createElement("li");
+    newEl.textContent = inputValue;
+    
+    taskList.appendChild(newEl);
+}
+
+let form = document.querySelector("#task-form");
+
+form.addEventListener("submit", addTask);
