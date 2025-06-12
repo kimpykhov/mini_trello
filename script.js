3bdeb0d9a8a5c23f2ebd85389cf1af432180e207
Kim Pykhov <pykhovk@gmail.com>
2025-06-12 22:13:57 +0300

    fixed bugs, added uuid generator + field to obj, added support to filter btns

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
 // TODO vars visibility
 
-// TODO ID to identify in cards const?
-
 // TODO functions for creating in DOM tree elements depend of const completed/active
 
 // TODO better way to identify status? mb with an integer?
@@ -18,10 +16,12 @@ function addTask(event) {
     let inputValue = document.querySelector("#task-input").value;
 
     let tasksList;
+    let uuid = self.crypto.randomUUID();
 
     const active = 1;
     const completed = 0;
     const cards = {
+        id: uuid,
         description: inputValue,
         status: active
     };
@@ -51,9 +51,12 @@ function addTask(event) {
     domTaskList.appendChild(newEl);
 }
 
+// load tasks after page reboot 
 function loadTasks() {
     const stored = localStorage.getItem("tasks");
+
     let parsedList = [];
+
     if (stored != null) {
        parsedList = JSON.parse(stored);
        let domTaskList = document.querySelector("#task-list");
@@ -65,9 +68,8 @@ function loadTasks() {
     }
 }
 
-function deleteTasks(event) {    
-    event.preventDefault();
-
+// tasks removal
+function deleteTasks(event) { 
     let tree = document.querySelectorAll("#task-list li");
 
     localStorage.clear();
@@ -75,11 +77,38 @@ function deleteTasks(event) {
     tree.forEach((element) => element.remove());
 }
 
+//filter tasks by typeof
+function filterTasks(event) {
+    
+    const clickedId = event.target.id;
+
+    switch (clickedId) {
+        case "all-btn":
+            console.log("all");
+            break;
+        case "active-btn":
+            console.log("active");
+            break;
+        case "completed-btn":
+            console.log("completed");
+            break;
+        default:
+            console.log("unknown")
+    }
+}
+
 let form = document.querySelector("#task-form");
 
+// buttons declaration
 const deleteBtn = document.querySelector("#delete-btn");
+const allBtn = document.querySelector("#all-btn");
+const activeBtn = document.querySelector("#active-btn");
+const completedBtn = document.querySelector("#completed-btn");
 
 form.addEventListener("submit", addTask);
 deleteBtn.addEventListener("click", deleteTasks);
+allBtn.addEventListener("click", filterTasks);
+activeBtn.addEventListener("click", filterTasks);
+completedBtn.addEventListener("click", filterTasks);
 
 loadTasks();
